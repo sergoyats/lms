c4535c4f7423ac94ab39ece178161a4ca355a9d8
sergoyats <abiens-abi@outlook.com>
2020-05-05 21:05:33 +0300

    add Q objects

--- src/group/views.py
+++ src/group/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
+from django.db.models import Q
 from group.models import Group
 
+
 def groups_list(request):
-    qs = Group.object.all()
-    Name = request.GET.get('name')
-    Specialty = request.GET.get('spec')
-    Number = request.GET.get('number')
+	qs = Group.object.all()
+	name = request.GET.get('name')
+	specialty = request.GET.get('spec')
+	number = request.GET.get('number')
 
-    if Name or Specialty or Number:
-        qs = qs.filter((name=Name) | (spec=Specialty) | (number=Number))
+	qs = qs.filter(Q(name=name) | Q(spec=specialty) | Q(number=number))
 
-    result = '<br>'.join(str(group) for group in qs)
+	result = '<br>'.join(str(group) for group in qs)
 
-    return render(request=request,
+	return render(request=request,
                   template_name='groups_list.html',
                   context={'groups_list': result}
                   )
