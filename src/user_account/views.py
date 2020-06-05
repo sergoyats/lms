2854c43e069d9f349ce014716fb178ad32993d13
sergoyats <abiens-abi@outlook.com>
2020-06-05 16:46:57 +0300

    login, logout, admin panel

--- src/user_account/views.py
+++ src/user_account/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from user_account.forms import UserAccountRegistrationForm
+from user_account.forms import UserAccountRegistrationForm, UserAccountProfileForm
 
 
 class CreateUserAccountView(CreateView):
@@ -17,3 +18,22 @@ class CreateUserAccountView(CreateView):
         context = super().get_context_data(object_list=None, **kwargs)
         context['title'] = 'Register new user'
         return context
+
+
+class UserAccountLoginView(LoginView):
+    template_name = 'login.html'
+    extra_context = {'title': 'Login as a user'}
+
+
+class UserAccountLogoutView(LogoutView):
+    template_name = 'logout.html'
+    extra_context = {'title': 'Logout from LMS'}
+
+
+class UserAccountProfileView(UpdateView):
+    template_name = 'profile.html'
+    extra_context = {'title': 'Edit current user profile'}
+    form_class = UserAccountProfileForm
+
+    def get_object(self):
+        return self.request.user
